21de401a84c8a649a4fa4c4ec15063dd5b7e4833
christina smith <christina@christinaharris.design>
2018-08-08 10:33:11 -0700

    Add files via upload

---
+++ lesson-2-html-and-css-fundamentals-1-3/js/index.js
@@ -0,0 +1,43 @@
+/*
+Exercise I - Selector Quiz
+Time: 20 Minutes
+
+START HERE
+
+Answer the questions below based on the HTML / CSS to the left. Do not manipulate the HTML or CSS in any way until after you have completed the quiz (no peeking!). Afterwards you may delete line 1 from the CSS to check your work (reminder this is not graded! Try and compelte this without looking). 
+
+1. Examine the h3 element containing "Step 1". What is the text color? What is the background color?
+
+The text color is aqua and the background color is black.
+
+2. Look at the h3 element containing "Step 3". What is the text and background color of this element?
+
+The text color is orange. The background color isn't specified.
+
+3. Look at all four paragraphs. What is the text color of each of them?
+
+All paragraphs have a lightgray text color.
+
+4. What is the border color around the first div element? 
+
+The border is red
+
+5. What is the background color for the second div element?
+
+background color isn's specified. Its automatically set to white
+
+6. What is the background color for each of the three p elements with the class "step-text"?
+
+The background color is blue under step-text class.
+
+
+Stretch Questions:
+
+7. What is the text and background color of the h1 element?
+
+h1 has a default of black text and a white background
+
+8. Of the p elements, which is the largest (takes up the most space on the page)?
+
+final-step-text looks like it takes up the most space on the page
+*/
